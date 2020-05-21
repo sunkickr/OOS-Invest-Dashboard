aa6da458d9290982c6c29cdf646663dc6f4cf32b
dkoenit <davidkoenit@gmail.com>
2020-05-20 20:05:27 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sunkickr/OOS-Invest-Dashboard/precache-manifest.5a7d04f7d3654a235f1c17cef0a3448a.js"
+  "/OOS-Invest-Dashboard/precache-manifest.4f15b9d145c880453503f8fdc85df243.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sunkickr/OOS-Invest-Dashboard/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/OOS-Invest-Dashboard/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
